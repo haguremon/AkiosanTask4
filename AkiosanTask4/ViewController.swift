305c0ccc74009ad26d34c752343405507f03e947
Yuta iwasaki <yanyong0127@gmail.com>
2021-07-06 16:24:03 +0900

    よろしくお願いしますー

--- AkiosanTask4/ViewController.swift
+++ AkiosanTask4/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet private var numberLabel: UILabel!
+    var numberCount = 0 //一時的に格納するー
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.numberLabel.text = String(numberCount) //最初の画面で0を表示するー
     }
-
-
+//+1ボタンが押された時にnumberCountに＋１するー
+    @IBAction func countUp(_ sender: UIButton) {
+        
+        numberCount += 1
+        numberLabel.text = String(numberCount)
+    
+    }
+//clearボタンが押された時にnumberCountにゼロを代入するー
+    @IBAction func clearButton(_ sender: UIButton) {
+        numberCount = 0
+        numberLabel.text = String(numberCount)
+    
+    }
+    
 }
 
